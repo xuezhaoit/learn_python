1cc2de58fe19b31b68dfa090de89fb8b81633e8c
xuezhao <ghw947209263@163.com>
2021-06-24 07:31:09 +0800

    20210624

--- python_3_fun.py
+++ python_3_fun.py
@@ -32,15 +32,41 @@
 # print( isinstance("0.22",(int,float)) )
 
 # 开根号
-import math
-print(math.sqrt(2))
+# import math
+# print(math.sqrt(2))
 
-# 练习
-def quadratic(a, b, c):
-    x1 = (-b + math.sqrt(b*b-4*a*c))/2*a
-    x2 = (-b - math.sqrt(b*b-4*a*c))/2*a
-    return x1, x2
+# # 练习
+# def quadratic(a, b, c):
+#     x1 = (-b + math.sqrt(b*b-4*a*c))/2*a
+#     x2 = (-b - math.sqrt(b*b-4*a*c))/2*a
+#     return x1, x2
 
-# print('quadratic(2, 3, 1) =', quadratic(2, 3, 1))
-print('quadratic(1, 3, -4) =', quadratic(1, 3, -4))
+# # print('quadratic(2, 3, 1) =', quadratic(2, 3, 1))
+# print('quadratic(1, 3, -4) =', quadratic(1, 3, -4))
 
+# ===========函数参数===============
+
+
+# ============递归函数=============
+# 注意防止栈溢出 fact(1000)
+# def fact(n):
+#     if n==1:
+#         return 1
+#     return n * fact(n - 1)
+
+# print (fact(1000))
+
+# 尾递归优化,只是做了优化，但还是会导致栈溢出
+def fact(n):
+    return fact_iter(n, 1)
+
+def fact_iter(num, products):
+    if num == 1:
+        return products;
+    return fact_iter(num-1, num*products)
+
+# print(fact_iter(10, 1))
+print(fact_iter(1000, 1))
+        
+        
+    
