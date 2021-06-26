de6d3ccaa53e9d7d2083f22b220f0320b70b6af2
xuezhao <ghw947209263@163.com>
2021-06-26 15:47:01 +0800

    20210626

--- python_4_AdvancedFeatures.py
+++ python_4_AdvancedFeatures.py
@@ -36,12 +36,41 @@
 #     print(ch)
 
 # 判断对象是否是迭代对象
-# 方法是通过collections.abc模块的Iterable类型判断
-from collections.abc import Iterable
-print(isinstance('abc', Iterable))
+# # 方法是通过collections.abc模块的Iterable类型判断
+# from collections.abc import Iterable
+# print(isinstance('abc', Iterable))
 
-# Python内置的enumerate函数可以把一个list变成索引-元素对
-for i, value in enumerate(['A', 'B', 'C']):
-    print(i, value)
+# # Python内置的enumerate函数可以把一个list变成索引-元素对
+# for i, value in enumerate(['A', 'B', 'C']):
+#     print(i, value)
     
+# ===========列表生成器================
+# [变脸 for item1 in d 条件1]
+# [m + n for m in 'ABC' for n in 'XYZ']
+# 等效于
+L = []
+for chr1 in "ABC":
+    for chr2 in "XYZ":
+        L.append(chr1+chr2)
+print(L)   
+print([chr1+chr2 for chr1 in "ABC" for chr2 in "XYZ" ]) 
+# if 语句
+print( [x for x in range(10) if x % 2 == 0] )
+# if else 语句
+print( [ x if x % 2 == 0 else 0 for x in range(10) ] )
+
+# ===========列表生成器================
+# 这种一边循环一边计算的机制，称为生成器：generator
+# 创建generator
+# 创建L和g的区别仅在于最外层的[]和()，L是一个list，而g是一个generator。
+g = (x*x for x in range(10))
+# 打印 generator
+# 方法1
+# print(next(g))
+# print(next(g))
+# print(next(g))
+# 方法2
+for item in g:
+    print(item)
     
+
